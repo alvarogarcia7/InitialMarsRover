29f18156e8805506433ddc59da27ed834358ff01
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 11:52:19 +0200

    red: move forwards (as long as it is facing north, in 0,0)
    #scaffold

---
+++ src/main/java/com/dodevjutsu/kata/marsrover/MarsRover.java
@@ -0,0 +1,10 @@
+package com.dodevjutsu.kata.marsrover;
+
+public class MarsRover {
+    public MarsRover(int x, int y, String direction) {
+    }
+
+    public String receive(String commands) {
+        return null;
+    }
+}
---
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
@@ -0,0 +1,13 @@
+package com.dodevjutsu.kata.marsrover;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MarsRoverMovementShould {
+    @Test
+    public void move_forward_pointing_north() throws Exception {
+        assertThat(new MarsRover(0, 0, "N").receive("f"), is(new MarsRover(0, 1, "N")));
+    }
+}
