683459bc2c4f7c763dbe2e27aae95451a6ce1d42
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 14:37:16 +0200

    refactor: rename test class

--- src/test/java/com/dodevjutsu/kata/marsrover/RotateRightShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/RotateRightShould.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class DirectionTurnRightShould {
+public class RotateRightShould {
 
     @Test
     public void when_pointing_north() throws Exception {
