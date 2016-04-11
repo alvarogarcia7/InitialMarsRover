96a4edce841fbcad645bc7b3fa40eb1d388d0524
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 12:03:06 +0200

    red: move backward pointing north
    we modify the command, not the orientation, yet

--- src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
@@ -10,4 +10,9 @@ public class MarsRoverMovementShould {
     public void move_forward_pointing_north() throws Exception {
         assertThat(new MarsRover(0, 1, "N").receive("f"), is(new MarsRover(0, 2, "N")));
     }
+
+    @Test
+    public void move_backward_pointing_north() throws Exception {
+        assertThat(new MarsRover(0, 1, "N").receive("b"), is(new MarsRover(0, 0, "N")));
+    }
 }
