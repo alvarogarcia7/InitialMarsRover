2596aada2122324872dfd0943583fcad9ea13de0
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 12:53:56 +0200

    add hashCode, equals to new type

--- src/main/java/com/dodevjutsu/kata/marsrover/Direction.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/Direction.java
@@ -14,4 +14,20 @@ public class Direction {
     public String value() {
         return value;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Direction direction = (Direction) o;
+
+        return value != null ? value.equals(direction.value) : direction.value == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
+    }
 }
