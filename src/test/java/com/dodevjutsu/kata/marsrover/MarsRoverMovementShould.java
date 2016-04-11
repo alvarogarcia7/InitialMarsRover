b4a874f5af70651a27740eb8c9f76588c8944397
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 12:05:50 +0200

    refactor: remove duplicated code for rover creation

--- src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
@@ -1,18 +1,27 @@
 package com.dodevjutsu.kata.marsrover;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
 public class MarsRoverMovementShould {
+
+    private MarsRover rover;
+
+    @Before
+    public void setUp() throws Exception {
+        rover = new MarsRover(0, 1, "N");
+    }
+
     @Test
     public void move_forward_pointing_north() throws Exception {
-        assertThat(new MarsRover(0, 1, "N").receive("f"), is(new MarsRover(0, 2, "N")));
+        assertThat(rover.receive("f"), is(new MarsRover(0, 2, "N")));
     }
 
     @Test
     public void move_backward_pointing_north() throws Exception {
-        assertThat(new MarsRover(0, 1, "N").receive("b"), is(new MarsRover(0, 0, "N")));
+        assertThat(rover.receive("b"), is(new MarsRover(0, 0, "N")));
     }
 }
