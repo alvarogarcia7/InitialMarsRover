74f388d311d5333907e413a64446249bdb8f6519
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 12:01:46 +0200

    refactor: this test is unnecessary

--- src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
@@ -10,11 +10,4 @@ public class MarsRoverMovementShould {
     public void move_forward_pointing_north() throws Exception {
         assertThat(new MarsRover(0, 1, "N").receive("f"), is(new MarsRover(0, 2, "N")));
     }
-
-    @Test
-    public void move_forward_pointing_north_wide_range() throws Exception {
-        for(int i=-1000; i<1000; i++) {
-            assertThat(new MarsRover(0, i, "N").receive("f"), is(new MarsRover(0, i+1, "N")));
-        }
-    }
 }
