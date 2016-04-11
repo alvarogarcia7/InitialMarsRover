bba4827849b54815303be91d678384933b688f56
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 11:54:55 +0200

    green

--- src/main/java/com/dodevjutsu/kata/marsrover/MarsRover.java
+++ src/main/java/com/dodevjutsu/kata/marsrover/MarsRover.java
@@ -1,10 +1,30 @@
 package com.dodevjutsu.kata.marsrover;
 
 public class MarsRover {
+    private int y;
+
     public MarsRover(int x, int y, String direction) {
+        this.y = y;
     }
 
-    public String receive(String commands) {
-        return null;
+    public MarsRover receive(String commands) {
+        this.y = 1;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MarsRover marsRover = (MarsRover) o;
+
+        return y == marsRover.y;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return y;
     }
 }
