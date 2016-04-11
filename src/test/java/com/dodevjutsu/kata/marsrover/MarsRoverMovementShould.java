0277e322f69efe4b787c02933aa79582b26032be
Alvaro Garcia <derecerca@gmail.com>
2016-04-11 11:58:29 +0200

    refactor: remove repeated test

--- src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverMovementShould.java
@@ -8,11 +8,6 @@ import static org.hamcrest.core.Is.is;
 public class MarsRoverMovementShould {
     @Test
     public void move_forward_pointing_north() throws Exception {
-        assertThat(new MarsRover(0, 0, "N").receive("f"), is(new MarsRover(0, 1, "N")));
-    }
-
-    @Test
-    public void move_forward_pointing_north_from_another_point() throws Exception {
         assertThat(new MarsRover(0, 1, "N").receive("f"), is(new MarsRover(0, 2, "N")));
     }
 }
