60e27883c5086b3ef103fc1f69f440fcc9772479
Alvaro Garcia <derecerca@gmail.com>
2016-04-12 17:25:10 +0200

    refactor: extract method for creating MarsRover instantiation
    #factoryMethod

--- src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverShould.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/MarsRoverShould.java
@@ -12,19 +12,25 @@ public class MarsRoverShould {
 
     @Test
     public void not_move_when_receiving_an_empty_command_sequence() throws Exception {
-        final MarsRover rover = new MarsRover(0, 0, "N");
+        final MarsRover rover = marsRoverAt(0, 0, "N");
 
         rover.receive("");
 
-        assertThat(rover, is(new MarsRover(0, 0, "N")));
+        assertThat(rover, is(marsRoverAt(0, 0, "N")));
     }
 
     @Test
     public void moving_forward_when_facing_north() throws Exception {
-        final MarsRover rover = new MarsRover(0, 0, "N");
+        final MarsRover rover = marsRoverAt(0, 0, "N");
 
         rover.receive("f");
 
-        assertThat(rover, is(new MarsRover(0, 1, "N")));
+        assertThat(rover, is(marsRoverAt(0, 1, "N")));
     }
+
+    private MarsRover marsRoverAt(int x, int y, String direction) {
+        return new MarsRover(x, y, direction);
+    }
+
+
 }
