1c88e7829249b01ae523d1c24e606ed98eb6b7de
Alvaro Garcia <derecerca@gmail.com>
2016-04-13 17:35:22 +0200

    Refactor: Rename class

--- src/test/java/com/dodevjutsu/kata/marsrover/RoverTest.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/RoverTest.java
@@ -4,11 +4,13 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class CanaryTest {
+public class RoverTest {
     @Test
     public void ReceiveEmptySequence(){
         Rover rover = new Rover(1, 2, "N");
+
         rover.execute("");
+
         assertEquals(new Rover(1, 2, "N"), rover);
     }
 
