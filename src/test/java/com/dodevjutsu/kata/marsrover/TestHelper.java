04661af5ff3e57795613fdfc3f17fb782c7eaf20
Alvaro Garcia <derecerca@gmail.com>
2016-04-19 08:07:01 +0200

    refactor: prefer using existing method
    #dry

--- src/test/java/com/dodevjutsu/kata/marsrover/TestHelper.java
+++ src/test/java/com/dodevjutsu/kata/marsrover/TestHelper.java
@@ -2,7 +2,7 @@ package com.dodevjutsu.kata.marsrover;
 
 public class TestHelper {
     static MarsRover marsRoverAt(int x, int y, String direction) {
-        return new MarsRover(x, y, direction, new InfiniteWorld());
+        return marsRoverAt(x, y, direction, new InfiniteWorld());
     }
 
     static MarsRover marsRoverAt(int x, int y, String direction, World world) {
